129ceaf364e6dadc08ea9c96673bb9fd8f7548b2
Ekaterina <seemenkina@gmail.com>
2019-10-27 15:36:48 +0700

    do chl12

--- set2/set2.go
+++ set2/set2.go
@@ -3,7 +3,9 @@ package set2
 import (
 	"bytes"
 	"crypto/rand"
+	"encoding/base64"
 	"fmt"
+	"github.com/davecgh/go-spew/spew"
 	"github.com/seemenkina/cryptopals/set1"
 )
 
@@ -175,7 +177,7 @@ func EncryptionOracle(input []byte) ([]byte, error) {
 
 func BlackBox(oracle functionOracle) {
 	const size = 64
-	input := make([]byte, 64)
+	input := make([]byte, size)
 	for i := 0; i < 20; i++ {
 		out, _ := oracle(input)
 		if set1.HasRepeatedBlock(out) {
@@ -184,4 +186,33 @@ func BlackBox(oracle functionOracle) {
 			fmt.Println("CBC")
 		}
 	}
+}
+
+func Chl12() []byte {
+	const key = "6e666eccf758c5386fb3b444ba61c362"
+	const unknownStr = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lyb" +
+		"GllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK"
+	unkRaw, _ := base64.StdEncoding.DecodeString(string(unknownStr))
+	encr, _ := ByteAtTimeECBDetect(unkRaw, []byte(key))
+	return encr
+}
+
+func ByteAtTimeECBDetect(secret []byte, randKey []byte) ([]byte, error) {
+	var suf = []byte("A")
+	var input []byte
+	for i := 0; i < 16; i++ {
+		suf = append(suf, []byte("A")...)
+		input = append(suf, secret...)
+		if len(input)%16 == 0 {
+			println(len(input) / 16)
+			break
+		}
+	}
+	input, _ = AddPKCS7Pad(input, 16)
+	encr, _ := set1.AES128ECB(randKey, input)
+	if set1.HasRepeatedBlock(encr) {
+		fmt.Println("ECB")
+	}
+	spew.Dump(encr)
+	return encr, nil
 }
